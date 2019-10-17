b601a6b894a14613de804317d2d1e408cfa70a79
futsuki <gcc111014@gmail.com>
2019-10-17 17:01:21 +0900

    Suppress UPM type warning

--- Runtime/Triggers/AsyncMouseTrigger.cs
+++ Runtime/Triggers/AsyncMouseTrigger.cs
@@ -1,5 +1,5 @@
 
-#if !(UNITY_IPHONE || UNITY_ANDROID || UNITY_METRO)
+//#if !(UNITY_IPHONE || UNITY_ANDROID || UNITY_METRO)
 
 #if CSHARP_7_OR_LATER || (UNITY_2018_3_OR_NEWER && (NET_STANDARD_2_0 || NET_4_6))
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
@@ -126,4 +126,4 @@ namespace UniRx.Async.Triggers
 #endif
 
 
-#endif
+//#endif
